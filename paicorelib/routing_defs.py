7f0c4a1aaec06f6088521c83abc741df3d27f1ad
Zachariah0318 <2965517804@qq.com>
2025-04-22 18:39:02 +0800

    Restore code formatting.

--- paicorelib/routing_defs.py
+++ paicorelib/routing_defs.py
@@ -23,6 +23,7 @@ __all__ = [
 # The base coordinate of online cores
 ONLINE_CORES_BASE_COORD = 0b11100_10100 if HwParams.COORD_Y_PRIORITY else 0b10100_11100
 
+
 @unique
 class RoutingLevel(IntEnum):
     """The level of routing node.
