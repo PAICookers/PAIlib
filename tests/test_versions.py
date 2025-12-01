0c31666233c62cedeb8ca46447384800b50ceb12
KafCoppelia <k740677208@gmail.com>
2025-12-01 17:56:47 +0800

    ✅ test(version): add numpy version check

---
+++ tests/test_versions.py
@@ -0,0 +1,13 @@
+import paicorelib
+import sys
+
+
+def test_pailib_version():
+    assert isinstance(paicorelib.__version__, str) and paicorelib.__version__ >= "0.0.1"
+
+
+def test_numpy_version_on_zynq():
+    if sys.platform == "armv7l":
+        import numpy as np
+
+        assert np.__version__ < "2.0.0"
