8722bc1d8cab6d5d7336b55f910220ac810108df
wenhhjj <1305857443@qq.com>
2025-12-27 22:08:43 +0800

    加入2.5lib库的core参数和neuron参数

---
+++ paicorelib/core_defs.py
@@ -0,0 +1,172 @@
+from enum import IntEnum, unique
+
+__all__ = [
+    "CoreLim",
+    "SNNMode",
+    "PoolingMode",
+    "PotentialAddMode",
+    "ZeroOutputMode",
+    "InputSignMode",
+    "InputWidth",
+    "OutputSignMode",
+    "OutputWidth",
+    "WeightSignMode",
+    "WeightWidth",
+    "LCNMode",
+    "CSCAccelerateMode",
+]
+
+
+class CoreLim:
+    """Core parameter limits."""
+    AXON_SKEW_MIN = -32768
+    AXON_SKEW_MAX = 32767
+    NEURON_NUMBER_MAX = 4096
+    TEST_CORE_OFFSET_MIN = -31
+    TEST_CORE_OFFSET_MAX = 31
+    GLOBAL_SEND_MAX = 127
+    GLOBAL_RECEIVE_MAX = 63
+    THREAD_NUMBER_MAX = 1023
+    BUSY_CYCLE_MAX = 4095
+    DELAY_CYCLE_MAX = 65535
+    WIDTH_CYCLE_MAX = 255
+    TICK_START_MAX = 65535
+    TICK_DURATION_MAX = 4294967295
+    TICK_INITIALIZER_MAX = 65535
+
+
+@unique
+class SNNMode(IntEnum):
+    """SNN and ANN mode selection.
+    0: SNN mode, using LIF operation rules;
+    1: ANN mode, using activation function operation rules;
+    """
+    SNN = 0
+    ANN = 1
+
+
+@unique
+class PoolingMode(IntEnum):
+    """Pooling mode selection.
+    0: Average pooling;
+    1: Max pooling;
+    """
+    AVERAGE = 0
+    MAX = 1
+
+
+@unique
+class PotentialAddMode(IntEnum):
+    """Accumulation mode selection.
+    0: Normal accumulation;
+    1: Direct accumulation of membrane potential;
+    """
+    NORMAL = 0
+    DIRECT_POTENTIAL = 1
+
+
+@unique
+class ZeroOutputMode(IntEnum):
+    """Whether to output zero values.
+    0: Do not output zero values;
+    1: Output zero values;
+    """
+    DISABLE = 0
+    ENABLE = 1
+
+
+@unique
+class InputSignMode(IntEnum):
+    """Input data sign selection.
+    0: Input is unsigned;
+    1: Input is signed;
+    """
+    UNSIGNED = 0
+    SIGNED = 1
+
+
+@unique
+class InputWidth(IntEnum):
+    """Input data bit width selection.
+    00: Input is 1-bit;
+    01: Input is 2-bit;
+    10: Input is 4-bit;
+    11: Input is 8-bit;
+    """
+    WIDTH_1BIT = 0
+    WIDTH_2BIT = 1
+    WIDTH_4BIT = 2
+    WIDTH_8BIT = 3
+
+
+@unique
+class OutputSignMode(IntEnum):
+    """Output data sign selection.
+    0: Output is unsigned;
+    1: Output is signed;
+    """
+    UNSIGNED = 0
+    SIGNED = 1
+
+
+@unique
+class OutputWidth(IntEnum):
+    """Output data bit width selection.
+    00: Output is 1-bit;
+    01: Output is 2-bit;
+    10: Output is 4-bit;
+    11: Output is 8-bit;
+    """
+    WIDTH_1BIT = 0
+    WIDTH_2BIT = 1
+    WIDTH_4BIT = 2
+    WIDTH_8BIT = 3
+
+
+@unique
+class WeightSignMode(IntEnum):
+    """Weight data sign selection.
+    0: Weight is unsigned;
+    1: Weight is signed;
+    """
+    UNSIGNED = 0
+    SIGNED = 1
+
+
+@unique
+class WeightWidth(IntEnum):
+    """Weight data bit width selection.
+    00: Weight is 1-bit;
+    01: Weight is 2-bit;
+    10: Weight is 4-bit;
+    11: Weight is 8-bit;
+    """
+    WIDTH_1BIT = 0
+    WIDTH_2BIT = 1
+    WIDTH_4BIT = 2
+    WIDTH_8BIT = 3
+
+
+@unique
+class LCNMode(IntEnum):
+    """Control the scale of fan-in extension.
+    1x is 512 fan-in / input bit width.
+    """
+    LCN_1X = 0
+    LCN_2X = 1
+    LCN_4X = 2
+    LCN_8X = 3
+    LCN_16X = 4
+    LCN_32X = 5
+    LCN_64X = 6
+    LCN_128X = 7
+
+
+@unique
+class CSCAccelerateMode(IntEnum):
+    """CSC compressed calculation acceleration mode.
+    0: No acceleration, compressed storage calculation is slow;
+    1: Acceleration, compressed storage calculation is faster, but vjt_initial needs to store weight_address_start again;
+    """
+    DISABLE = 0
+    ENABLE = 1
---
+++ paicorelib/neuron_defs.py
@@ -0,0 +1,161 @@
+from enum import IntEnum, unique
+
+__all__ = [
+    "OutputType",
+    "FoldType",
+    "NeuronType",
+    "ResetMode",
+    "ThresholdNegMode",
+    "ThresholdPosMode",
+    "LateralInhibitionMode",
+    "LeakMultiSequence",
+    "LeakMultiInputMode",
+    "LeakMultiMode",
+    "LeakAddMode",
+    "WeightCompressMode",
+    "NeuronLim",
+]
+
+
+class NeuronLim:
+    """Neuron parameter limits."""
+    TICK_RELATIVE_MAX = 255
+    ADDR_AXON_MAX = 511
+    ADDR_CORE_OFFSET_MIN = -31
+    ADDR_CORE_OFFSET_MAX = 31
+    WEIGHT_SKEW_MAX = 65535
+    WEIGHT_ADDRESS_MAX = 4095
+    RESET_V_MIN = -32768
+    RESET_V_MAX = 32767
+    LEAK_TAU_MIN = -32
+    LEAK_TAU_MAX = 31
+    LEAK_V_MIN = -524288
+    LEAK_V_MAX = 524287
+    VJT_INITIAL_MIN = -2048
+    VJT_INITIAL_MAX = 2047
+    FOLD_RANGE_MAX = 2047
+    FOLD_SKEW_MAX = 2047
+    FOLD_AXON_MAX = 2047
+    FOLD_NUMBER_MAX = 536870911
+
+
+@unique
+class OutputType(IntEnum):
+    """Output type selection.
+    0: Output spike or activation value;
+    1: Output membrane potential;
+    """
+    SPIKE_OR_ACTIVATION = 0
+    POTENTIAL = 1
+
+
+@unique
+class FoldType(IntEnum):
+    """Fold type selection.
+    0: Unfolded neuron;
+    1: Folded neuron;
+    """
+    UNFOLDED = 0
+    FOLDED = 1
+
+
+@unique
+class NeuronType(IntEnum):
+    """Neuron type selection.
+    0: Half neuron mode, occupies 128 bits;
+    1: Full neuron mode, occupies 256 bits;
+    """
+    HALF = 0
+    FULL = 1
+
+
+@unique
+class ResetMode(IntEnum):
+    """Reset mode selection.
+    00: Fixed reset (hard reset) mode;
+    01: Subtraction reset (soft reset) mode;
+    10: No reset mode;
+    """
+    FIXED = 0
+    SUBTRACTION = 1
+    NO_RESET = 2
+
+
+@unique
+class ThresholdNegMode(IntEnum):
+    """Negative threshold mode selection.
+    0: Fire mode;
+    1: Floor mode;
+    """
+    FIRE = 0
+    FLOOR = 1
+
+
+@unique
+class ThresholdPosMode(IntEnum):
+    """Positive threshold mode selection.
+    0: Fire mode;
+    1: Ceiling mode;
+    """
+    FIRE = 0
+    CEILING = 1
+
+
+@unique
+class LateralInhibitionMode(IntEnum):
+    """Lateral inhibition mode selection.
+    0: No lateral inhibition;
+    1: With lateral inhibition, if the current core generated a spike in the last time step, membrane potential accumulates from reset_v;
+    """
+    DISABLE = 0
+    ENABLE = 1
+
+
+@unique
+class LeakMultiSequence(IntEnum):
+    """Multiplicative leak sequence.
+    0: Leak before threshold comparison;
+    1: Leak after threshold comparison reset;
+    """
+    BEFORE_COMPARE = 0
+    AFTER_RESET = 1
+
+
+@unique
+class LeakMultiInputMode(IntEnum):
+    """Whether input participates in multiplicative leak.
+    0: Input does not participate in multiplicative leak;
+    1: Input participates in multiplicative leak;
+    """
+    DISABLE = 0
+    ENABLE = 1
+
+
+@unique
+class LeakMultiMode(IntEnum):
+    """Multiplicative leak mode selection.
+    0: No multiplicative leak, but membrane potential can be shifted;
+    1: With multiplicative leak;
+    """
+    DISABLE = 0
+    ENABLE = 1
+
+
+@unique
+class LeakAddMode(IntEnum):
+    """Additive leak mode selection.
+    0: Forward leak mode;
+    1: Backward leak mode;
+    """
+    FORWARD = 0
+    BACKWARD = 1
+
+
+@unique
+class WeightCompressMode(IntEnum):
+    """Weight type.
+    0: Dense mode, weight data uncompressed;
+    1: Sparse mode, weight data CSC compressed;
+    """
+    DENSE = 0
+    SPARSE = 1
